0ecf83514b42dedf9a659a919fbea0e99d58e409
Lorena <lorena.giddens@outlook.com>
2018-06-02 15:40:08 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,11 +8,12 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  # \w stands for any word character (letter, number, underscore), * => zero or more of
   text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
